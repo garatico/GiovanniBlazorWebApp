99e52eb806a85309f8e592637c4a8ee016a37ed2
Giovanni Aratico <garatico@asu.edu>
2023-01-04 00:32:31 -0700

    Styling fixes, folder organization

---
+++ Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GiovanniBlazorWebAppE.Models
+{
+    public class Album
+    {
+        public string albumTitle { get; set; }
+        public string albumDate { get; set; }
+        public string albumLink { get; set; }
+        public string albumImg { get; set; }
+        public string albumImgAlt { get; set; }
+        public string albumImgId { get; set; }
+    }
+}
+
+
+
